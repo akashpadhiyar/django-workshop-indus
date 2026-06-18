8a0bbcde8f456ac7f28cca12320f1db49a6d733b
Akash Padhiyar <akashpadhiyar@users.noreply.github.com>
2026-06-18 12:12:17 +0530

    Day 3-4
    Day 3-4

--- myuserapp/urls.py
+++ myuserapp/urls.py
@@ -6,8 +6,18 @@ urlpatterns = [
     path('home',views.homepage),
     path('about',views.aboutpage),    
     path('contact',views.contactpage), 
+    path('contactprocess',views.contactprocess), 
     path('cake',views.contactpage), 
     path('cake/ahmedabad',views.contactpage), 
     path('cake/rajkot',views.contactpage),    
+    path('shop',views.shoppage),
+    path('saveSession',views.saveSessionData),
+    path('getSession',views.getSessionData),
+    path('getSession2',views.getSessionData2),
+    path('removeSession',views.deleteSessionData),
     
+    path('login',views.loginpage),
+    path('loginprocess',views.loginprocess),
+    path('dashboard',views.dashboard),
+    path('logout',views.logout)
 ]
--- myuserapp/views.py
+++ myuserapp/views.py
@@ -1,9 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 # Create your views here.
 def homepage(request):
     return render(request,"home.html")
 def aboutpage(request):
     return render(request,"about.html")
+
+def shoppage(request):
+    return render(request,'shop.html')
+
 def contactpage(request):
     return render(request,"contact.html")
+
+def saveSessionData(request):
+    request.session.set_expiry(6)
+    request.session['username'] = "Akash Padhiyar"
+    return HttpResponse("Session Created")
+
+
+def getSessionData(request):
+    if request.session.has_key('username'):
+        time  = request.session.get_expiry_age()
+        print(time)
+        msg = request.session['username']
+        return HttpResponse(msg)
+    else:
+        return HttpResponse("Session Variable not Present ")
+
+def deleteSessionData(request):
+    del request.session['username'] 
+    return HttpResponse("Session Removed")
+
+def getSessionData2(request):
+    msg = request.session['username']
+    return HttpResponse(msg)
+
+def contactprocess(request):
+    a = int(request.POST['txt1'])
+    b = int(request.POST['txt2'])
+    c = a + b
+    msg = "A Value is",a,"B Value is ",b,"Sum is ",c
+    d = ""
+    if c == 30:
+        d = "If Condition Called"
+    elif c<30:
+        d = "Elseif Called"
+    else:
+        d= "Else Called"
+    return render(request,'ans.html',{'mya':a,'myb':b,'myc':c,'myd':d})
+
+def loginpage(request):
+    return render(request,'login.html')
+def loginprocess(request):
+    txt1 = request.POST['email']
+    request.session['myemail'] = txt1
+    return redirect(dashboard)
+
+def dashboard(request):
+    if request.session.has_key('myemail'):
+        return render(request,"dashboard.html")
+    else:
+        return redirect(loginpage)
+def logout(request):
+    del request.session['myemail']
+    return redirect(loginpage)
